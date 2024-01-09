28a91bf6250dc3d78b6dc353561d32a2992ba116
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-01-08 23:36:14 -0300

    creating hooks

--- src/components/BookingForm/BookingForm.tsx
+++ src/components/BookingForm/BookingForm.tsx
@@ -1,57 +1,13 @@
 import React from 'react';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Typography } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
-import { Controller, useForm } from 'react-hook-form';
-import dayjs, { Dayjs } from 'dayjs';
-import { useRecoilState } from 'recoil';
-import { generateUniqueId } from '@/utils';
-import { IBooking } from '@/interfaces/booking';
-import { bookingsState } from '@/states/booking';
-
-export const bookingSchema = z.object({
-  id: z.string().optional(),
-  checkIn: z.custom((val) => dayjs.isDayjs(val), {
-      message: "Check-in must be a valid date.",
-  }).nullable(),
-  checkOut: z.custom((val) => dayjs.isDayjs(val), {
-      message: "Check-out must be a valid date.",
-  }).nullable(),
-})
-
-export type BookingFormInput = z.infer<typeof bookingSchema>;
+import { Controller } from 'react-hook-form';
+import dayjs from 'dayjs';
+import { useBookingForm } from '@/hooks/useBookingForm';
 
 export function BookingForm({ defaultValues = {} }) {
-  const { control, handleSubmit, formState: { errors }, watch, setError } = useForm<BookingFormInput>({
-    resolver: zodResolver(bookingSchema),
-    defaultValues,
-  });
-  const [bookings, setBookings] = useRecoilState<IBooking[]>(bookingsState);
-
-  const onSubmit = (data: BookingFormInput) => {
-    const checkInDate = dayjs.isDayjs(data.checkIn) ? data.checkIn as Dayjs : null;
-    const checkOutDate = dayjs.isDayjs(data.checkOut) ? data.checkOut as Dayjs : null;
-
-    const bookingExists = bookings.some(booking =>
-        booking.id !== data.id &&
-        booking.checkIn?.isSame(checkInDate, 'day') && 
-        booking.checkOut?.isSame(checkOutDate, 'day')
-    );
-
-    if (bookingExists) {
-        setError('checkIn', { type: 'manual', message: 'Booking with these dates already exists!' });
-        setError('checkOut', { type: 'manual', message: 'Booking with these dates already exists!' });
-    } else {
-        if (data.id) {
-            setBookings(bookings.map(booking => booking.id === data.id ? { ...booking, checkIn: checkInDate, checkOut: checkOutDate } : booking));
-        } else {
-            const newBooking = { id: generateUniqueId(), checkIn: checkInDate, checkOut: checkOutDate };
-            setBookings([...bookings, newBooking]);
-        }
-    }
-  }
+  const { control, handleSubmit, errors, watch, onSubmit } = useBookingForm(defaultValues);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
--- src/features/Booking.tsx
+++ src/features/Booking.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
-import { bookingsState } from '@/states/booking';
-import { useRecoilState } from 'recoil';
 import { IBooking } from '@/interfaces/booking';
 import { BookingCard } from '@/components/BookingCard';
 import { BookingForm } from '@/components/BookingForm';
-import { useModal } from '@/hooks/useModal';
 import { Header } from '@/components/Header';
+import { useBooking } from '@/hooks/useBooking';
 
 export function Booking() {
-  const [bookings, setBookings] = useRecoilState<IBooking[]>(bookingsState);
-  const { openModal } = useModal();
-
-  const handleOpen = (editingBooking: IBooking) => {
-    openModal({
-        body:
-          <BookingForm 
-            defaultValues={{ checkIn: editingBooking?.checkIn, checkOut: editingBooking?.checkOut, id: editingBooking?.id }}
-          />
-    })
-  };
-
-  const handleDelete = (bookingToDelete: IBooking) => {
-    setBookings(bookings.filter(booking => booking !== bookingToDelete));
-  };
+  const { deleteBooking, editBooking, bookings } = useBooking();
 
   return (
         <div>
@@ -34,8 +18,8 @@ export function Booking() {
                         <BookingCard.CheckinLabel date={booking.checkIn} />
                         <BookingCard.CheckoutLabel date={booking.checkOut} />
                         <BookingCard.Actions>
-                            <BookingCard.Edit onEdit={() => handleOpen(booking)} />
-                            <BookingCard.Delete onDelete={() => handleDelete(booking)} />
+                            <BookingCard.Edit onEdit={editBooking} />
+                            <BookingCard.Delete onDelete={deleteBooking} />
                         </BookingCard.Actions>
                     </BookingCard>
                 ))}
