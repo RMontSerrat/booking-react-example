40263a5316e6896ec328e7a2d41e754d9a5f9231
Romulo Mont Serrat <romulo.serrat@barkyn.com>
2024-01-23 13:29:05 -0300

    serialize

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,15 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   transform: {
     // Transformar arquivos `.ts` ou `.tsx` usando `babel-jest`
-    '^.+\\.(ts|tsx)$': 'babel-jest',
+    "^.+\\.(ts|tsx)$": "babel-jest",
   },
+  snapshotSerializers: [
+    "@emotion/jest/serializer" /* if needed other snapshotSerializers should go here */,
+  ],
   moduleNameMapper: {
     // Mapear importações de arquivos estáticos e CSS Modules
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1',  // Mapeia '@' para a pasta 'src'
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1", // Mapeia '@' para a pasta 'src'
   },
 };
